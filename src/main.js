1ac34600b439c7ad6ce6c80549f8a5f918cb63ca
xwzsd <shamik2024@gmail.com>
2019-02-11 00:21:58 +0200

    main_js_axios

--- src/main.js
+++ src/main.js
@@ -1,8 +1,37 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from "vue"
+import firebase from 'firebase'
+import './plugins/vuetify'
+import App from "./App.vue"
 
+import axios from 'axios'
+
+import router from './router'
+import store from './store'
+import BootstrapVue from "bootstrap-vue"
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+axios.defaults.baseURL = 'https://vue-face.firebaseio.com/'
+// axios.defaults.headers.common['Authorization'] = 'fasfdsa'
+axios.defaults.headers.get['Accepts'] = 'application/json'
+
+Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
 
+// Initialize Firebase
+const config = {
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASE_URL,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID
+};
+
+firebase.initializeApp(config);
+
 new Vue({
+  router,
+  store,
   render: h => h(App)
 }).$mount("#app");
